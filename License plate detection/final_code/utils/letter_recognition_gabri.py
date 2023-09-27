dddc190275eaf5b579809719b3a2a069985b1185
Gabriel Gausachs <g4a4b4r4i@gmail.com>
2023-09-27 17:54:26 +0200

    pytesseract gabri

--- License plate detection/final_code/utils/letter_recognition_gabri.py
+++ License plate detection/final_code/utils/letter_recognition_gabri.py
@@ -1,1 +1,21 @@
 import pandas as pd
+import pytesseract
+import cv2
+import time
+
+#-----------------USING PYTESSERACT DIRECTLY WITH THE LICENSE PLATE-------------------
+
+pytesseract.pytesseract.tesseract_cmd=r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+
+image = cv2.imread('C:/Users/Gabriel/OneDrive/Escritorio/4t any uni/psiv/PSIV-projects/License plate detection/final_code/img/matricula.jpg')
+
+# Run tesseract OCR on image
+text = pytesseract.image_to_string(image)
+
+# Print recognized text
+print(text)
+
+
+#----------------ANOTHER APPROACH----------------
+
+
