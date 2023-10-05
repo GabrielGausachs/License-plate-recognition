b45bfc2b04c6cac3d5454a79ed1c16fc40467844
Sergi Garriga <garriga2002@gmail.com>
2023-10-05 17:40:48 +0200

    Only one deep test

--- License plate detection/tests/test_charac_segmentator.py
+++ License plate detection/tests/test_charac_segmentator.py
@@ -7,7 +7,7 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from utils.characters_segmentator import segmentate_characters
+from ..utils.characters_segmentator import segmentate_characters
 
 if __name__ == "__main__":
     segmentate_characters("plate_test.png")
